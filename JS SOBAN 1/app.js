3e1e481f7078a943b763f60098c4c5954edcb58d
Sobanhussain786 <hussainsubhan876@gmail.com>
2025-09-19 15:18:29 +0530

    JS ASSIGNMENT-1

---
+++ JS SOBAN 1/app.js
@@ -0,0 +1,209 @@
+ // CHAPTER 1   
+   
+//   Q2
+//  alert("Error!Please enter a valid password");
+
+// Q3
+//   alert("Welcome to JS Land \n Happy Coding!");
+
+// Q4
+// alert("Welcome to JS Land");
+// alert("Happy Coding");
+
+// Q5
+//  var names="Hello I can run JS through my web browser console";
+// console.log(names);
+
+// Q6
+// alert("Welcome to the My Website");
+
+            //   CHAPTER 2
+
+// Q1    
+// var user="username";
+// console.log(user);
+
+// Q2
+//  var fullName="Soban ";
+//  var lastName="M.Hussain"
+//  console.log(fullName,lastName);
+
+// Q3 (a)
+// var title="message";
+// console.log(title);
+
+// (b) 
+// var message="Hello World";
+// console.log(message);
+
+// (c)
+// var message="Hello World";
+// alert(message);
+
+// Q.4
+//  var names="Bilal Hussain";
+//  alert(names);
+//  var age="15 years old";
+//  alert(age);
+//  var qualification="Certified Mobile Application Development";
+//  alert(qualification);
+
+// Q.5
+// var names="PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(names);
+
+// Q.6
+// var names="My email address is ";
+// var email="example@example.com";
+// alert(names+""+email);
+
+// Q.7
+// var book="A smarter way to learn javascrpit";
+// alert(book);
+
+// Q.8
+// document.write("Yah! I can write HTML content through JAVASCRIPT");
+
+// Q9
+// var names="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(names);
+
+              //    CHAPTER 3
+
+// Q1
+// var age="I AM 15 Year Old";
+// alert(age);
+
+// Q2
+// var visit="You have visited this site 15 times";
+// alert (visit);
+
+// Q3
+// document.write("My Birth Year is 2007");
+
+// Q4
+// document.write("John Doe Ordered 5T-shirt(s) on XYZ clothing store");
+
+                 // CHAPTER 4
+
+// Q1
+// var names,age,email;
+
+// Q2
+// var username,user123,age,$price,_city;
+// var 123username,@email,124,user name,user-name;
+
+// Q3
+// document.write("Variable names can only contain,numbers,$ and_For example:e $my_1stVariable"); 
+
+// document.write("Variable names can only contain,numbers,$ and_For example:e $my_1stVariable ");
+
+
+                 // // CHAPTER 5
+
+// Q1
+// var num1=6;
+// var num2=8;
+// var sum=num1+num2
+// document.write(sum);
+
+// Q2  (FOR SUBTRACTION)
+// var num1=12;
+//  var num2=8;
+//  var sum=num1-num2;
+//  document.write(sum)
+
+// (FOR MULTIPLICATION)
+//  var num1=12;
+//  var num2=8;
+//  var sum=num1*num2;
+//  document.write(sum);
+
+//  (FOR DIVISION)
+//  var num1=12;
+//  var num2=4;
+//  var sum=num1/num2;
+//  document.write(sum);
+
+// (FOR MODULUS)
+//  var num1=12;
+//  var num2=8;
+//  var sum=num1%num2;
+//  document.write(sum);
+
+// Q3 (a)
+// var sum;
+// document.write(sum + "<br>");
+// // (b)
+// var num = 8;
+// document.write(num + "<br>");
+// // (c)
+// var num4=5;
+// document.write("initial value is "+ num4 +"<br>" );
+// // (d)
+// var g=5 + ++x;
+// document.write("increment value is "+ g );
+
+// Q5
+// var userInput = Number(prompt("Enter a number"));
+
+// document.write(userInput, " x 1 = ", userInput * 1, "<br>",
+//     userInput, " x 2 = ", userInput * 2, "<br>",
+//     userInput, " x 3 = ", userInput * 3, "<br>",
+//     userInput, " x 4 = ", userInput * 4, "<br>",
+//     userInput, " x 5 = ", userInput * 5, "<br>",
+//     userInput, " x 6 = ", userInput * 6, "<br>",
+//     userInput, " x 7 = ", userInput * 7, "<br>",
+//     userInput, " x 8 = ", userInput * 8, "<br>",
+//     userInput, " x 9 = ", userInput * 9, "<br>",
+//     userInput, " x 10 = ", userInput * 10, "<br>"
+// );
+
+// Q6
+// var celsiusTemp = prompt("Enter celsius temperature");
+
+// var FahrenheitTemp = prompt("Enter Fahrenheit temperature");
+
+// var convertF = (celsiusTemp * 9) / 5 + 32;
+
+// var convertC = ((FahrenheitTemp - 32) * 5) / 9;
+
+// console.log("Fahrenheit ", convertF);
+
+// console.log("Celsius ", convertC);
+
+
+            // CHAPTER.06-09
+
+// Q#2 
+//  var a =2,b =1; 
+//  var c =--a;
+//  console.log(c);
+
+//  var d = --a - --b;
+//  console.log(d);
+
+//  var e=--a - --b + ++b;
+//  console.log(e);
+
+//  var f=--a - --b + ++b + b--;
+//  console.log(f);
+
+// Q#3
+//  var hello="Hello User";
+//  alert(hello);
+//  var username =prompt("Enter User Name");
+
+// Q#5
+//  var userInput = Number(prompt("Enter a number") || 5);
+
+//  document.write(userInput, " x 1 = ", userInput * 1, "<br>",
+//      userInput, " x 2 = ", userInput * 2, "<br>",
+//     userInput, " x 3 = ", userInput * 3, "<br>",
+//     userInput, " x 4 = ", userInput * 4, "<br>",
+//     userInput, " x 5 = ", userInput * 5, "<br>",
+//     userInput, " x 6 = ", userInput * 6, "<br>",
+//     userInput, " x 7 = ", userInput * 7, "<br>",
+//      userInput, " x 8 = ", userInput * 8, "<br>",
+//      userInput, " x 9 = ", userInput * 9, "<br>",
+//      userInput, " x 10 = ", userInput * 10, "<br>");
