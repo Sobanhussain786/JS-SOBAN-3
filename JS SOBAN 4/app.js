9b971b6334cc12cd620e5dfb7275aa90d7de987b
Sobanhussain786 <hussainsubhan876@gmail.com>
2025-09-19 15:23:39 +0530

    JS-ASSIGNMENT-4

---
+++ JS SOBAN 4/app.js
@@ -0,0 +1,49 @@
+  //    CHAP#18-22
+
+// Q3
+// for(var a=1; a<=10 ;a++){
+//     document.write(a,"<br>")
+// };
+
+// Q4
+// var num1=+prompt("Enter a number for multiplication");
+// var num2=+prompt("Enter lenght of table");
+// for(var i=1;i<=num2;i++){
+//     document.write(num1,"x",i,"=",num1 *i,"<br>")    
+// };
+
+// Q5
+// var fruit=["apple","banana","mango","orange","strawberry"];
+// document.write(fruit.slice(0,5));
+
+// Q7
+// var search=+prompt("Welcome to ABC bakery.What do you want to order sir/madam");
+// var matchFound="no";
+// itemsArr=["cake","apple pie","cookies","chips","patties"];
+
+// for(var i=0;i< itemsArr.lenght;i++){
+//     if(search===itemsArr[i]){
+//         matchFound="yes";
+//         document.write("Avaliable");
+//     }
+// }
+//     if(matchFound==="no"){
+//         document.write("We are sorry.It is not avaliable");
+//     }
+
+// Q8
+// numArray=[24,53,78,91,12];
+// document.write("The largest number is ", numArray.slice(3,4));
+
+// Q9
+// numArray=[24,53,78,91,12];
+// document.write("The smallest number is ", numArray.slice(4,5));
+
+// Q10
+// var range=[5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
+// document.write(range.slice(0,20));
+   
+// Q1
+// var firstName=prompt("Enter your first name");
+// var lastName=prompt("Entr your last name");
+// document.write(firstName + lastName);
